c051692373f50f7dfaa805891e46748008a572bc
Benedikt Spies <benedikt.spies@gmail.com>
2021-11-12 03:40:34 +0100

    server loads certificate from file

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ It is recommended to increase the maximum buffer size by running (See https://gi
 ```
 sysctl -w net.core.rmem_max=2500000
 ```
+
+## Generate Self-signed certificate
+```
+openssl req -x509 -nodes -out server.crt -keyout server.key -config server.req
+```
--- server/server.go
+++ server/server.go
@@ -3,17 +3,12 @@ package server
 import (
 	"bufio"
 	"context"
-	"crypto/rand"
-	"crypto/rsa"
 	"crypto/tls"
-	"crypto/x509"
-	"encoding/pem"
 	"fmt"
 	"github.com/lucas-clemente/quic-go"
 	"github.com/lucas-clemente/quic-go/logging"
 	"github.com/lucas-clemente/quic-go/qlog"
 	"io"
-	"math/big"
 	"net"
 	"os"
 	"qperf-go/common"
@@ -21,8 +16,6 @@ import (
 )
 
 // Run server
-//
-// The sync.WaitGroup may be nil
 func Run(addr net.UDPAddr, createQLog bool, migrateAfter time.Duration) {
 
 	state := common.State{}
@@ -48,7 +41,18 @@ func Run(addr net.UDPAddr, createQLog bool, migrateAfter time.Duration) {
 		Tracer: multiTracer,
 	}
 
-	listener, err := quic.ListenAddr(addr.String(), generateTLSConfig(), &conf)
+	//TODO make CLI option
+	tlsCert, err := tls.LoadX509KeyPair("server.crt", "server.key")
+	if err != nil {
+		panic(err)
+	}
+
+	tlsConf := tls.Config{
+		Certificates: []tls.Certificate{tlsCert},
+		NextProtos:   []string{"qperf"},
+	}
+
+	listener, err := quic.ListenAddr(addr.String(), &tlsConf, &conf)
 	if err != nil {
 		panic(err)
 	}
@@ -100,27 +104,5 @@ func sendData(stream quic.SendStream, sessionId uint64) {
 			fmt.Printf("[session %d][stream %d] %s\n", sessionId, stream.StreamID(), err)
 			return
 		}
-	}
-}
-
-func generateTLSConfig() *tls.Config {
-	key, err := rsa.GenerateKey(rand.Reader, 1024)
-	if err != nil {
-		panic(err)
-	}
-	template := x509.Certificate{SerialNumber: big.NewInt(1)}
-	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
-	if err != nil {
-		panic(err)
-	}
-	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
-	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
-	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
-	if err != nil {
-		panic(err)
-	}
-	return &tls.Config{
-		Certificates: []tls.Certificate{tlsCert},
-		NextProtos:   []string{"qperf"},
 	}
 }
